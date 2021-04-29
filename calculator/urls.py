5df1fb54b19d452a988dc44d801ed751377c9869
ShivanS93 <shivan.sivakumaran@gmail.com>
2021-04-30 10:49:46 +1200

    created templates

--- calculator/urls.py
+++ calculator/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.index, name='index')
+        path('', views.index, name='index'),
+        path('minimal_lens_thickness/', views.minimal_lens_thickness, name='minimal_lens_thickness'),
         ]
--- calculator/views.py
+++ calculator/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Testing calculator')
+    return render(request=request, template_name='calculator/index.html')
+
+def minimal_lens_thickness(request):
+    return render(request=request, template_name='calculator/minimum_lens_thickness.html')
