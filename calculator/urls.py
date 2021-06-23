0ca78de09d1d3f74fa1965cdd05bd69de06eff47
ShivanS93 <shivan.sivakumaran@gmail.com>
2021-06-23 22:36:55 +1200

    new navbar made

--- calculator/urls.py
+++ calculator/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import IndexView, MinimumBlankSizeView, MBSCalculate
 
 urlpatterns = [
-        path('', IndexView.as_view()),
-        path('minimum_blank_size/', MinimumBlankSizeView.as_view()),
+        path('', IndexView.as_view(), name='index'),
+        path('minimum_blank_size/', MinimumBlankSizeView.as_view(), name='minimum_blank_size'),
         path('mbs_calculate/', MBSCalculate.as_view()),
         ]
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-#!usr/bin/env bash
+#!/bin/bash
 poetry run python3 manage.py runserver 8001
