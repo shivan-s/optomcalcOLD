f1ac2786e47532fe9349776db83e437ae6afb95d
ShivanS93 <shivan.sivakumaran@gmail.com>
2021-06-27 00:26:03 +1200

    working with mono_pds

--- calculator/views.py
+++ calculator/views.py
@@ -1,6 +1,7 @@
 import logging
 import math
 import os
+from typing import NamedTuple
 
 from django.shortcuts import render
 from django.views import View 
@@ -16,7 +17,16 @@ class MinimumBlankSizeView(TemplateView):
     template_name = 'calculator/minimum_blank_size.html'
     
 class MBSCalculate(View):
+    def __init__(self):
+        self.bino_pd = 0
+    
     def post(self, request):
+        def _minimum_blank_size(mono_pd: float):
+            """
+            Calculates minimal blank size given a monocular pd
+            """
+            decentration = abs( (frame_pd / 2) - mono_pd)
+            return decentration + effective_diameter + 2
 
         names = { 
                 'right_pd': 'Right PD',
@@ -29,7 +39,9 @@ class MBSCalculate(View):
         ref = (self.request.POST).dict()
         
         logging.info(ref) # all of the post data - form inputs
-        
+
+        if ref['right_pd']:
+            self.bino_pd = float(ref['right_pd'])
         if not all(ref.values()):
             # checking for invalid values (nulls) and listing them
             err = [names[k] for k, v in ref.items() if not v]
@@ -45,15 +57,11 @@ class MBSCalculate(View):
             frame_size = ref['frame_size']
             frame_dbl = ref['frame_dbl']
             effective_diameter = ref['effective_diameter']
-        # source of information - https://www.insightnews.com.au/finding-the-minimum-lens-blank-size-part-2-2/
+            # source of information - https://www.insightnews.com.au/finding-the-minimum-lens-blank-size-part-2-2/
             frame_pd = frame_size + frame_dbl
-            def minimum_blank_size(mono_pd: float):
-                """Calculates minimal blank size given a monocular pd"""
-                decentration = abs( (frame_pd / 2) - mono_pd)
-                return decentration + effective_diameter + 2
-
-            right_mbs = minimum_blank_size(right_pd)
-            left_mbs = minimum_blank_size(left_pd)
+             
+            right_mbs = _minimum_blank_size(right_pd)
+            left_mbs = _minimum_blank_size(left_pd)
 
             answer = f"""
             <table class="table table-striped table-hover"
@@ -73,3 +81,11 @@ class MBSCalculate(View):
             # TODO: create checks and ranges for appropriate sizes
             # TODO: if mono pd is above 45, then split this between left and right
         return HttpResponse(answer)
+    
+    def get(self, request):
+        right_pd = 30
+        left_pd = 30
+        if self.bino_pd:
+            right_pd = self.bino_pd / 2
+            left_pd = self.bino_pd / 2
+        return HttpResponse(right_pd, left_pd)
